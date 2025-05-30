18afbfcef50a2cdfae366eb01e11ca41d8a0b495
Jahmal Richard <jahmalrichardpro@gmail.com>
2025-05-30 07:25:07 -0400

    clean up

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,22 +1,3 @@
-// 'use client'
-
-// import { useThemeContext } from './ThemeProvider'
-// import { Moon, Sun } from 'lucide-react'
-
-// export default function ThemeToggle() {
-//   const { theme, toggleTheme } = useThemeContext()
-
-//   return (
-//     <button
-//       onClick={toggleTheme}
-//       className="p-2 rounded-md text-xl hover:text-blue-400"
-//       aria-label="Toggle Theme"
-//     >
-//       {theme === 'dark' ? <Sun className="w-5 h-5" /> : <Moon className="w-5 h-5" />}
-//     </button>
-//   )
-// }
-
 "use client";
 
 import { useTheme } from "next-themes";
